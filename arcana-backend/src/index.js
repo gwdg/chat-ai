2adc4081b56d5f1fbf4986c76586446f0fa7d50e
p.chikhaliya <p.chikhaliya@stud.uni-goettingen.de>
2024-08-20 18:19:05 +0200

    initial setup arcana backend

---
+++ arcana-backend/src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Import routes
+const apiRoutes = require("./routes/apiRoutes");
+app.use("/api", apiRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
