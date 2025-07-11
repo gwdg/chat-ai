054bb2ed96c612963e0b604d9f806e674ed2bd9e
p.chikhaliya <p.chikhaliya@stud.uni-goettingen.de>
2025-07-11 18:50:50 +0200

    Auto refresh with rag model fixed

--- front/src/components/Markdown/ReferencesSection.jsx
+++ front/src/components/Markdown/ReferencesSection.jsx
@@ -370,6 +370,11 @@ const ProgressiveReferenceItem = memo(
               <div className="w-4 h-4" />
             )}
           </div>
+          {!hasContent && (
+            <span className="text-xs text-gray-400 dark:text-gray-500 ml-2">
+              (title only)
+            </span>
+          )}
         </div>
 
         {/* Expanded content */}
